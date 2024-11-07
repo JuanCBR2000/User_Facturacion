a673d7e10a6c67f7089a377823563e5200ff42d0
Juan C Barrios <juancarlosbarriosreyes@gmail.com>
2024-11-06 19:22:05 -0600

    Primer Comit Proyecto Facturacion

---
+++ Facturacion_Demo/Facturacion_Demo/Controllers/Factura.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Facturacion_Demo.Controllers
+{
+    public class Factura : Controller
+    {
+        public IActionResult IndexFactura()
+        {
+            return View();
+        }
+    }
+}
